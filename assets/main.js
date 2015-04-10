e829a8ac51fe851c3e7a3558075fcfe32d052d07
SpyTec <spytec13@gmail.com>
2015-04-11 00:26:19 +0200

    Added logging

--- assets/main.js
+++ assets/main.js
@@ -43,7 +43,9 @@ function get_systems_within_radius (current_system, radius) {
 	console.log(systems_within_radius);
 }
 function is_system_within_radius (cur_system, system, radius) {
-	if(radius > Math.sqrt( Math.pow((system.x - cur_system.x), 2) + Math.pow((system.y - cur_system.y), 2) + Math.pow((system.z - cur_system.z), 2)))
+	vector = Math.sqrt( Math.pow((system.x - cur_system.x), 2) + Math.pow((system.y - cur_system.y), 2) + Math.pow((system.z - cur_system.z), 2));
+	console.log(vector);
+	if(radius > vector)
 	{
 		return true;
 	}
