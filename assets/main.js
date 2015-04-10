febe90a4bc77f4bc5078cb1f1edfd89d3ceab813
SpyTec <spytec13@gmail.com>
2015-04-11 00:12:26 +0200

    Added search radius algorithm

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,49 @@
 jQuery(document).ready(function($) {
 	init();
-	if(check_system('Kelish'))
-	{
-		alert("It exists");
-	}
 });
+
 function init () {
 	$.bmdata = {};
 	load_systems();
 	load_stations();
+}
+
+$("#current_system").submit(function(e) {
+	e.preventDefault();
+
+	var start_system = $("#start_system").val;
+	var radius = $("#ly_radius").val;
+
+	if(check_system(start_system))
+	{
+		var systems [];
+		systems = get_systems_within_radius(start_system, radius);
+	}
+	else
+	{
+		alert("System doesn't exist");
+		//Add feedback to user
+	}
+});
+
+function get_systems_within_radius (current_system, radius) {
+	var current_system_data = [];
+	var systems_within_radius = [];
+
+	current_system_data = getObjects($.bmdata.systems, 'name', current_system);
+	$.each($.bmdata.systems, function(index, system) {
+		if(is_system_within_radius(current_system_data, system, radius))
+		{
+			$(systems_within_radius).add(system);
+		}
+	});
+}
+function is_system_within_radius (cur_system, system, radius) {
+	if(radius > sqrt( Math.pow((system.x - cur_system.x), 2) + Math.pow((system.y - cur_system.y), 2) + Math.pow((system.z - cur_system.z), 2)))
+	{
+		return true;
+	}
+	return false;
 }
 function load_systems() {
 	$.bmdata = { systems:[] };
