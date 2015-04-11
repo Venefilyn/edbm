4af42b40daed3fbb8ca7eaf9540ec129c01ba2df
SpyTec <spytec13@gmail.com>
2015-04-11 19:22:10 +0200

    Changed getObjects function to be case-insensitive

--- assets/main.js
+++ assets/main.js
@@ -138,7 +138,7 @@ function getObjects(obj, key, val) {
         if (!obj.hasOwnProperty(i)) continue;
         if (typeof obj[i] == 'object') {
             objects = objects.concat(getObjects(obj[i], key, val));
-        } else if (i == key && obj[key] == val) {
+        } else if (i == key && obj[key].toLowerCase() == val.toLowerCase()) {
             objects.push(obj);
         }
     }
