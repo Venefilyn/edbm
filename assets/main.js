2f22da46b1555697b3989be18679f03ec1d2c403
SpyTec <spytec13@gmail.com>
2015-04-12 19:08:45 +0200

    Fixed clearing of #results

--- assets/main.js
+++ assets/main.js
@@ -55,7 +55,7 @@ function get_systems_within_radius (current_system, radius) {
 	});
 
 	results = $("#results");
-	results.html();
+	results.html('');
 	results.append('<h2>Systems with black market</h2>');
 	results.append('<p>Found ' + systems_with_blackmarket.length + ' systems with a potential black market</p>');
 	results.append('<table class="table table-striped table-bordered"><thead><th>System</th><th>Station</th><th>Faction</th><th>Distance to star</th><th>Distance from star</th><th>Black Market</th></thead><tbody>');
